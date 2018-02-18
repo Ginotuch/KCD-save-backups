c17cc9cff7276de85d2588302f55d9f00321abc5
Ginotuch <6973573+Ginotuch@users.noreply.github.com>
2018-02-18 16:33:50 +1300

    added warning, and if more backups than maximum

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # KCD-save-backups
 This will automatically backup save games/profiles for the game Kingdom Come: Deliverance to avoid corruption.  
 Keeps all backups, or overwrites older ones.
+
+####Usage
+Change the amount of backups to keep in main.py (line 5). Set it to 0 to keep infinite backups.  
+Then either run the file, or from the command line: 
+
+#### Requirements
+* Python 3+ (Tested with Python 3.6.4)
+* Packages: Toml, Requests
--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import os
 from time import sleep
 import shutil
+import pathlib
 
-backup_size = 20  # 0 for infinite, otherwise maximum backups allowed. Number should always be integer 0 or greater
+backup_size = 1  # 0 for infinite, otherwise maximum backups allowed. Number should always be integer 0 or greater
 # Warning: backups can be large in file size, infinite backups can use up a lot of disk space
 
 
 def main():
-    save_location = "testfolders"
-    make_backup(save_location)
+    save_location = os.path.join(str(pathlib.Path.home()), "Saved Games")
+    if not "kingdomcome" in os.listdir(save_location):
+        print("kingdomcome SAVE FOLDER NOT FOUND IN", save_location)
+        print("Please launch the game and create a save before running this script")
+        print("If you think this message was shown in error, please make an issue at https://github.com/Ginotuch/KCD-save-backups")
+        input()
+        exit()
     max_time = get_max(save_location)
     if not os.path.exists(os.path.join(save_location, "backups")):
         os.mkdir(os.path.join(save_location, "backups"))
@@ -16,7 +22,7 @@ def main():
         if get_max(save_location)[1] > max_time[1]:
             max_time = get_max(save_location)
             make_backup(save_location)
-        sleep(120)
+        sleep(1)
 
 
 def make_backup(save_location):
@@ -29,8 +35,17 @@ def make_backup(save_location):
         shutil.copytree(source, destination)
 
     else:
+        while len(old_backups) > backup_size:
+            old_backups.sort()
+            shutil.rmtree(os.path.join(backups_loc, old_backups[0]))
+            old_backups.pop(0)
+            for backup in old_backups:
+                os.rename(os.path.join(backups_loc, backup),
+                          os.path.join(backups_loc, backup[:6] + str(int(backup[6:]) - 1)))
+            old_backups = [d for d in os.listdir(backups_loc) if os.path.isdir(os.path.join(backups_loc, d))]
+
         if len(old_backups) < backup_size:
-            new_name = str(max([int(x[6:]) for x in old_backups]) + 1)
+            new_name = "1" if len(old_backups) == 0 else str(max([int(x[6:]) for x in old_backups]) + 1)
             source = os.path.join(save_location, "kingdomcome")
             destination = os.path.join(save_location, "backups", "backup{}".format(new_name))
             shutil.copytree(source, destination)
@@ -41,7 +56,7 @@ def make_backup(save_location):
             for backup in old_backups:
                 os.rename(os.path.join(backups_loc, backup), os.path.join(backups_loc, backup[:6] + str(int(backup[6:]) - 1)))
             old_backups = [d for d in os.listdir(backups_loc) if os.path.isdir(os.path.join(backups_loc, d))]
-            new_name = str(max([int(x[6:]) for x in old_backups]) + 1)
+            new_name = "1" if len(old_backups) == 0 else str(max([int(x[6:]) for x in old_backups]) + 1)
             source = os.path.join(save_location, "kingdomcome")
             destination = os.path.join(save_location, "backups", "backup{}".format(new_name))
             shutil.copytree(source, destination)
