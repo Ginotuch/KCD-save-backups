cd7fe15698071ec61aa18c920779314fadb813ed
Ginotuch <6973573+Ginotuch@users.noreply.github.com>
2018-02-18 15:49:46 +1300

    custom backup count completed

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import os
 from time import sleep
 import shutil
 
-backup_size = 0  # 0 for infinite, otherwise maximum backups
+backup_size = 20  # 0 for infinite, otherwise maximum backups allowed. Number should always be integer 0 or greater
+# Warning: backups can be large in file size, infinite backups can use up a lot of disk space
 
 
 def main():
@@ -15,7 +16,7 @@ def main():
         if get_max(save_location)[1] > max_time[1]:
             max_time = get_max(save_location)
             make_backup(save_location)
-        sleep(1)
+        sleep(120)
 
 
 def make_backup(save_location):
@@ -27,6 +28,23 @@ def make_backup(save_location):
         destination = os.path.join(save_location, "backups", "backup{}".format(new_name))
         shutil.copytree(source, destination)
 
+    else:
+        if len(old_backups) < backup_size:
+            new_name = str(max([int(x[6:]) for x in old_backups]) + 1)
+            source = os.path.join(save_location, "kingdomcome")
+            destination = os.path.join(save_location, "backups", "backup{}".format(new_name))
+            shutil.copytree(source, destination)
+        elif len(old_backups) == backup_size:
+            old_backups.sort()
+            shutil.rmtree(os.path.join(backups_loc, old_backups[0]))
+            old_backups.pop(0)
+            for backup in old_backups:
+                os.rename(os.path.join(backups_loc, backup), os.path.join(backups_loc, backup[:6] + str(int(backup[6:]) - 1)))
+            old_backups = [d for d in os.listdir(backups_loc) if os.path.isdir(os.path.join(backups_loc, d))]
+            new_name = str(max([int(x[6:]) for x in old_backups]) + 1)
+            source = os.path.join(save_location, "kingdomcome")
+            destination = os.path.join(save_location, "backups", "backup{}".format(new_name))
+            shutil.copytree(source, destination)
 
 
 def get_max(save_location):
