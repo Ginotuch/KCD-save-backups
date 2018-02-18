09ba0dd126c81fd0e82623e158f86938072ee446
Ginotuch <6973573+Ginotuch@users.noreply.github.com>
2018-02-18 15:22:03 +1300

    completed infinite backup setting

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
 import os
+from time import sleep
+import shutil
+
+backup_size = 0  # 0 for infinite, otherwise maximum backups
 
 
 def main():
     save_location = "testfolders"
+    make_backup(save_location)
     max_time = get_max(save_location)
-    print(max_time)
+    if not os.path.exists(os.path.join(save_location, "backups")):
+        os.mkdir(os.path.join(save_location, "backups"))
+    while True:
+        if get_max(save_location)[1] > max_time[1]:
+            max_time = get_max(save_location)
+            make_backup(save_location)
+        sleep(1)
+
+
+def make_backup(save_location):
+    backups_loc = os.path.join(save_location, "backups")
+    old_backups = [d for d in os.listdir(backups_loc) if os.path.isdir(os.path.join(backups_loc, d))]
+    if backup_size == 0:
+        new_name = str(max([int(x[6:]) for x in old_backups]) + 1)
+        source = os.path.join(save_location, "kingdomcome")
+        destination = os.path.join(save_location, "backups", "backup{}".format(new_name))
+        shutil.copytree(source, destination)
+
 
 
 def get_max(save_location):
