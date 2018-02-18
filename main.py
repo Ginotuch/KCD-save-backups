8c1b484668b792abdd44f2bc454da53d712ba276
Ginotuch <6973573+Ginotuch@users.noreply.github.com>
2018-02-18 14:55:54 +1300

    added get_max: finding newest file

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 import os
+
+
+def main():
+    save_location = "testfolders"
+    max_time = get_max(save_location)
+    print(max_time)
+
+
+def get_max(save_location):
+    max_time = (0, 0)
+    save_location += "\\kingdomcome"
+    for folder_path, folder_names, file_names in os.walk(save_location):
+        for item in folder_names, file_names:
+            for a in item:
+                if os.path.getctime(os.path.join(folder_path, a)) > max_time[1]:
+                    max_time = (os.path.join(folder_path, a), os.path.getctime(os.path.join(folder_path, a)))
+    return max_time
+
+
+if __name__ == "__main__":
+    main()
